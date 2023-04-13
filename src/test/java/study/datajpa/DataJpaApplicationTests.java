032cf76b82cc12f103001ac33ad80e3144f378fa
이재영 <jaeyoung.yi@seculetter.com>
2023-04-13 09:57:12 +0900

    섹션 3. 공통 인터페이스 기능: 공통 인터페이스 설정

--- src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -2,9 +2,11 @@ package study.datajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootTest
+//@EnableJpaRepositories(basePackages = "datajpa.repository")
 class DataJpaApplicationTests {
 
 	@Test
