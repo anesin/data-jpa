ab6a0567216665527b606cb8fa2dadc3c5069d21
이재영 <jaeyoung.yi@seculetter.com>
2023-04-13 09:43:36 +0900

    섹션 3. 공통 인터페이스 기능: 순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -20,7 +22,29 @@ public class MemberJpaRepository {
   }
 
 
-  public Member findById(Long id) {
+  public void delete(Member member) {
+    em.remove(member);
+  }
+
+
+  public List<Member> findAll() {
+    return em.createQuery("select m from Member m", Member.class)
+             .getResultList();
+  }
+
+
+  public long count() {
+    return em.createQuery("select count(m) from Member m", Long.class)
+             .getSingleResult();
+  }
+
+
+  public Optional<Member> findById(Long id) {
+    return Optional.ofNullable(em.find(Member.class, id));
+  }
+
+
+  public Member find(Long id) {
     return em.find(Member.class, id);
   }
 
